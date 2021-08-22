de571df814f5b0cdf67cf84f19c90af29c58bcf3
Alegbe Mojisola <alegbeyemi@gmail.com>
2021-08-22 00:56:08 -0700

    changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,25 @@
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
 var currentSlide = 0;
 const slides = document.querySelectorAll(".slide")
 const dots = document.querySelectorAll('.dot')
-
 const init = (n) => {
   slides.forEach((slide) => {
     slide.style.display = "none"
